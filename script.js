e936358cdbb8adb61387a8520cd445362c878eb1
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-08-02 13:29:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let count = 1;
+
+
+function addItem(n) {
+	for(let i=0; i<n; i++){
+		let item = document.createElement('li');
+		item.textContent = `item ${n}`;
+		list.appendChild(item);
+	}
+}
+
+window.addEventListner('scroll", () => {
+	const scrollTop = window.scrollY;		
+    const viewportHeight = window.innerHeight;
+    const fullHeight = document.documentElement.scrollHeight
+
+    if (scrollTop + windowHeight >= fullHeight - 10) {
+    addItems(2); // Add 2 new items
+  }
+});
+addItems(10);
+
+
+
+
+
+
+
 
